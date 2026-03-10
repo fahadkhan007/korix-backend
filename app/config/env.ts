6f84b4eef1fdc8949e1e57b039e8e3734bf62d8c
fahad <fahadkhanf715@gmail.com>
2026-03-10 17:00:21 +0530

    added the cors origin for frontend client

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,4 +1,4 @@
 import { configDotenv } from "dotenv";
 configDotenv();
 
-export const { PORT, DATABASE_URL, JWT_SECRET, JWT_REFRESH_SECRET, REDIS_URL } = process.env;
+export const { PORT, DATABASE_URL, JWT_SECRET, JWT_REFRESH_SECRET, REDIS_URL, FRONTEND_CLIENT_URL } = process.env;
--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response, Application } from 'express';
 import cookieParser from 'cookie-parser';
 import authRouter from './app/routers/auth.router.js';
-import { PORT } from './app/config/env.js';
+import { PORT, FRONTEND_CLIENT_URL } from './app/config/env.js';
 import { connectRedis } from './app/database/redis.js';
 import { connectRedisRateLimit } from './app/config/ratelimitRedis.js';
 import errorMiddleware from './app/middlewares/error.middleware.js';
+import cors from 'cors';
+
 
 const app: Application = express();
 
+app.use(cors({
+    origin: FRONTEND_CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
