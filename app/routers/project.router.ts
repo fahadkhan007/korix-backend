5172d222f1ccea58d6d6028c81d6a6d45dbd9a44
fahad <fahadkhanf715@gmail.com>
2026-03-14 01:12:00 +0530

    clened some codes

--- app/routers/project.router.ts
+++ app/routers/project.router.ts
@@ -16,29 +16,21 @@ import {
 
 const router = Router();
 
-// Apply auth protecting and email verifying to ALL project routes
 router.use(protect);
 router.use(verifyEmailMiddleware);
 
-// Create top-level project & Get User's projects
 router.post('/', createProjectController);
 router.get('/', getProjectsByUserController);
 
-// View project details (Admin, Member, Viewer)
 router.get('/:projectId', requireProjectRole(ProjectRole.ADMIN, ProjectRole.MEMBER, ProjectRole.VIEWER), getProjectByIdController);
 
-// Check your own role inside a project
 router.get('/:projectId/role', requireProjectRole(ProjectRole.ADMIN, ProjectRole.MEMBER, ProjectRole.VIEWER), getMemberRoleController);
 
-// Manage Members (ADMIN ONLY)
 router.post('/:projectId/members', requireProjectRole(ProjectRole.ADMIN), addProjectMemberController);
 router.patch('/:projectId/members', requireProjectRole(ProjectRole.ADMIN), updateProjectMemberRoleController);
 
-// Sub-projects
-// To view sub-projects, you must be a member of the PARENT project
 router.get('/:projectId/subprojects', requireProjectRole(ProjectRole.ADMIN, ProjectRole.MEMBER, ProjectRole.VIEWER), getSubProjectsController);
 
-// To create a sub-project, you must be an ADMIN of the PARENT project
 router.post('/:projectId/subprojects', requireProjectRole(ProjectRole.ADMIN), createSubProjectController);
 
 export default router;
